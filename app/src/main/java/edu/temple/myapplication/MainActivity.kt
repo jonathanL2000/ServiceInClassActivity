7ec42d01bec1fd3b3cb2ccf1d9c79afb76cdb982
jonathan.lazarou <jonathan.lazarou@temple.edu>
2023-04-13 11:50:36 -0400

    implemented service connection object and binded timerservice. connected timer functions to ui buttons.

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -3,27 +3,51 @@ package edu.temple.myapplication
 import android.content.ComponentName
 import android.content.Intent
 import android.content.ServiceConnection
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
 import android.widget.Button
+import java.util.Timer
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var timerBinder: TimerService.TimerBinder
+    var isConnected = false
+
+    val serviceConnection = object : ServiceConnection  {
+        override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
+            timerBinder = p1 as TimerService.TimerBinder
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            isConnected = false
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bindService(Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
         findViewById<Button>(R.id.playButton).setOnClickListener {
-
+            if (isConnected) timerBinder.start(30)
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-
+            if (isConnected) timerBinder.pause()
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            if (isConnected) timerBinder.stop()
         }
+    }
+
+    override fun onDestroy() {
+        unbindService(serviceConnection)
+        super.onDestroy()
     }
 }
